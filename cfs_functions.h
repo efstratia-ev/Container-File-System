298f6c28c560782d4358c4cc5f126fc03a65f9ed
sdi1500038 <sdi1500038@di.uoa.gr>
2020-01-15 21:06:12 +0200

    arxi tou project

---
+++ cfs_functions.h
@@ -0,0 +1,9 @@
+#ifndef PROJECT4_CFS_FUNCTIONS_H
+#define PROJECT4_CFS_FUNCTIONS_H
+#include "cfs_file.h"
+
+cfs_file *cfs_workwith(char *filename);
+
+cfs_file *cfs_create(char *arguments);
+
+#endif
